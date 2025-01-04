abeee7c32b1ff3fc610880d2810949d2e3115bb1
MashaGoro21 <163156012+MashaGoro21@users.noreply.github.com>
2025-01-04 21:28:25 +0300

    Add Argon Assault project

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] GameObject deathFX;
+    [SerializeField] GameObject hitVFX;
+    [SerializeField] int scorePerHit = 15;
+    [SerializeField] int hitPoints = 4;
+
+    ScoreBoard scoreBoard;
+    GameObject parentGameObject;
+
+    void Start()
+    {
+        scoreBoard = FindObjectOfType<ScoreBoard>();
+        parentGameObject = GameObject.FindWithTag("Spawn At Runtime");
+        AddRigidbody();
+    }
+
+    void AddRigidbody()
+    {
+        Rigidbody rb = gameObject.AddComponent<Rigidbody>();
+        rb.useGravity = false;
+    }
+
+    void OnParticleCollision(GameObject other) 
+    {
+        ProcessHit();
+        if(hitPoints < 1)
+        {
+            KillEnemy();
+        }
+    }
+
+    void KillEnemy()
+    {
+        scoreBoard.IncreaseScore(scorePerHit);
+        GameObject vfx = Instantiate(deathFX, transform.position, Quaternion.identity);
+        vfx.transform.parent = parentGameObject.transform;
+        Destroy(gameObject);
+    }
+
+    void ProcessHit()
+    {
+        GameObject vfx = Instantiate(hitVFX, transform.position, Quaternion.identity);
+        vfx.transform.parent = parentGameObject.transform;
+        hitPoints--;
+    }
+}
